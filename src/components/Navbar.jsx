25c10636a68e57469de3f31a4bab67e90b744266
S. AASIM AHMED W <aasimahmedsiddiqui1@gmail.com>
2024-11-26 11:50:16 +0530

    navbat

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-blue-600 p-4 flex justify-between items-center fixed top-0">
+    <nav className="bg-blue-600 p-4 flex justify-between items-center fixed">
       <div className="text-white text-lg font-semibold">
         <Link to="/">GlobalConnect</Link>
       </div>
