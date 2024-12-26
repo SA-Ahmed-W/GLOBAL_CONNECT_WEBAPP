b75624f3ff1f6de6d81fa2097fd0308355aa980a
SUMANTHBEKAL <sumanthbekal505@gmail.com>
2024-12-26 14:57:01 +0530

    ahsdvjs

--- src/components/LocalTranslationAndSend.jsx
+++ src/components/LocalTranslationAndSend.jsx
@@ -112,6 +112,7 @@ function LocalTranslationAndSend({ callDocId, isCaller, peerConnection }) {
     [peerConnection, isDataChannelReady]
   );
 
+  
   useEffect(() => {
     const recognition = new SpeechRecognition();
     recognition.lang = inputLangCode || "en";// Adjust language as needed
